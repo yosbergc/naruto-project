4df41c7b80a0c28475930120d275b11e4e120399
Yosber Guerrero <yosbergc@gmail.com>
2024-10-21 19:58:35 -0400

    Added style to search component, added react-icons

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,12 @@
+import './search.css'
+import { IoSearch } from "react-icons/io5";
+
 function Search() {
  return (
-    <input type="text" />
+    <section className='search-container'>
+        <input type="text" className='header-main-search' placeholder='Your favorite characters, clans, akatsuki'/>
+        <IoSearch size={25}/>
+    </section>
  )
 }
 export { Search }
