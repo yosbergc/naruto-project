3ac361e32b1a903daaf347b307773a88c5721233
Yosber Guerrero <yosbergc@gmail.com>
2024-10-21 20:25:52 -0400

    Added filter

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,9 +3,17 @@ import { IoSearch } from "react-icons/io5";
 
 function Search() {
  return (
-    <section className='search-container'>
-        <input type="text" className='header-main-search' placeholder='Your favorite characters, clans, akatsuki'/>
-        <IoSearch size={25}/>
+    <section className='search-header-main-container'>
+        <section className='search-container'>
+        <input type="text" className='header-main-search' placeholder='Your favorite characters, clans, akatsukis'/>
+        <IoSearch size={25} style={{cursor: 'pointer'}}/>
+        </section>
+        <select name="type">
+            <option value="all">All</option>
+            <option value="characters">Character</option>
+            <option value="clans">Clan</option>
+            <option value="akatsukis">Akatsuki</option>
+        </select>
     </section>
  )
 }
